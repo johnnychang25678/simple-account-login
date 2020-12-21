e689687a8c6533e7db7865c3911fe78a128f2aab
Johnny Chang <zjps3407@gmail.com>
2020-12-21 18:12:21 +0800

    feat: add user authentication

--- app.js
+++ app.js
@@ -17,4 +17,46 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/login', (req, res) => {
+  const { email, password } = req.body
+  const users = [
+    {
+      firstName: 'Tony',
+      email: 'tony@stark.com',
+      password: 'iamironman'
+    },
+    {
+      firstName: 'Steve',
+      email: 'captain@hotmail.com',
+      password: 'icandothisallday'
+    },
+    {
+      firstName: 'Peter',
+      email: 'peter@parker.com',
+      password: 'enajyram'
+    },
+    {
+      firstName: 'Natasha',
+      email: 'natasha@gamil.com',
+      password: '*parol#@$!'
+    },
+    {
+      firstName: 'Nick',
+      email: 'nick@shield.com',
+      password: 'password'
+    }
+  ]
+  const message = authentication(users, email, password)
+  res.render('welcome', { message: message })
+})
+
 app.listen(port, () => console.log(`Listening to server on port: ${port}`))
+
+function authentication(users, email, password) {
+  for (const user of users) {
+    if (user.email === email && user.password === password) {
+      return `Welcome back, ${user.firstName}!`
+    }
+  }
+  return 'Email or password is incorrect!'
+}
