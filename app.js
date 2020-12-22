bdd73dc2691771c1b0c860861e6f8854b99f3c63
Johnny Chang <zjps3407@gmail.com>
2020-12-22 11:58:11 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 
 const app = express()
 const port = 3000
@@ -11,14 +12,35 @@ app.set('view engine', 'handlebars')
 // middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+app.use(session({ secret: 'mySecret', name: 'user', resave: false, saveUninitialized: false, cookie: { maxAge: 6000 } }))
+// a session will be stored in req.session for every request
 
 // routes
 app.get('/', (req, res) => {
+  if (req.session.user) {
+    return res.redirect('/welcome')
+  }
   res.render('index')
 })
 
+function auth(req, res, next) {
+  if (req.session.user) {
+    console.log('authenticated')
+    next()
+  } else {
+    console.log('not authenticated')
+    return res.redirect('/')
+  }
+
+}
+
+app.get('/welcome', auth, (req, res) => {
+  const userName = req.session.user
+  return res.render('welcome', { message: `Welcome back, ${userName}!` })
+})
+
+
 app.post('/login', (req, res) => {
-  const { email, password } = req.body
   const users = [
     {
       firstName: 'Tony',
@@ -46,17 +68,29 @@ app.post('/login', (req, res) => {
       password: 'password'
     }
   ]
-  const message = authentication(users, email, password)
-  res.render('welcome', { message: message })
+  const { email, password } = req.body
+  console.log(email, password)
+  if (email.trim() === '' || password.trim() === '') {
+    return res.render('index', { alert: 'Password or email is incorrect, please try again!' })
+  }
+
+  for (let user of users) {
+    if (user.email === email && user.password === password) {
+      req.session.user = user.firstName
+      return res.redirect('/welcome')
+    }
+  }
+
+  return res.render('index', { alert: 'Password or email is incorrect, please try again!' })
+
+})
+
+app.get('/logout', auth, (req, res) => {
+  req.session.destroy(() => {
+    console.log('session destroyed')
+  })
+  res.render('index', { alert: 'You are logged out! Re-enter email and password to log in again!' })
 })
 
 app.listen(port, () => console.log(`Listening to server on port: ${port}`))
 
-function authentication(users, email, password) {
-  for (const user of users) {
-    if (user.email === email && user.password === password) {
-      return `Welcome back, ${user.firstName}!`
-    }
-  }
-  return 'Email or password is incorrect!'
-}
